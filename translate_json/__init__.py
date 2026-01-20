54bb6dc7f52404779c9daf4a1f1a00a436d807c8
thienbd <thienbd@absoltech.vn>
2026-01-20 13:33:10 +0700

    chore: remove JSON translator module and web interface

--- translate_json/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Translate JSON module - Dịch file JSON sử dụng Argos Translate
-"""
-
-from .translator import JSONTranslator
-from .config import DEFAULT_INPUT, DEFAULT_OUTPUT
-
-__version__ = "1.0.0"
-__all__ = ["JSONTranslator", "DEFAULT_INPUT", "DEFAULT_OUTPUT"]
--- translate_json_local.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Simple script to translate JSON files using the translate_json module
-"""
-
-from translate_json.cli import main
-
-if __name__ == "__main__":
-    main()
